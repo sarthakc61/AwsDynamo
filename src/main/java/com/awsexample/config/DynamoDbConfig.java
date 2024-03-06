1b2f63c8f7943740c118c15b67f224dd50fefc03
sarthakc61 <48508711+sarthakc61@users.noreply.github.com>
2024-03-06 10:00:48 +0530

    removed actual secret key

--- src/main/java/com/awsexample/config/DynamoDbConfig.java
+++ src/main/java/com/awsexample/config/DynamoDbConfig.java
@@ -13,9 +13,9 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 @Configuration
 public class DynamoDbConfig {
 	
-	public static final String ACCESS_KEY = "AKIAXKFNDV3IPY5T7UGK";
+	public static final String ACCESS_KEY = "ReplaceWithAccessKey";
 	
-	public static final String SECRET_KEY = "+YpfVRNX27Tud16MuysDXYD8nO78J3w/c9xcBVuQ";
+	public static final String SECRET_KEY = "ReplaceWithSecurityKey";
 	public static final String SERVICE_ENDPOINT = "dynamodb.eu-north-1.amazonaws.com";
 	public static final String REGION = "eu-north-1";
 	@Bean
